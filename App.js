fe53a28e8c701ce39109f073a78b450b7bac7e60
Joaquin-Parrilla <joaquin.p.olivera@gmail.com>
2020-09-20 17:07:06 -0300

    App.js: Agrego funcion draw con setInterval

--- App.js
+++ App.js
@@ -3,10 +3,21 @@
 var canvas = document.getElementById('game');
 var ctx = canvas.getContext("2d");       // contexto de graficos 2d
 
-/* dibujar un cuadrado rojo: */
+/* dibujar un cuadrado rojo: 
 ctx.beginPath();    // inicio declaraciones de objeto
-ctx.rect(20, 40, 50, 50);
-ctx.fillStyle = "#0B2870";
-ctx.fill();
+ctx.rect(20, 40, 50, 50);       // defino un rectnagulo ("rect")
+ctx.fillStyle = "#0B2870";  // propiedad que guarda color que utilizará fill()
+ctx.fill();     // pintar cuadrado
 ctx.closePath();    // fin declaraciones de objeto
+*/
+
+function draw() {
+    ctx.beginPath();
+    ctx.arc(50, 50, 10, 0, Math.PI*2);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
+setInterval(draw, 10);  // cada 10ms
+
 
