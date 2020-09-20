732ede5daacb2ffed128eb4ecebab687d132c2c9
Joaquin-Parrilla <joaquin.p.olivera@gmail.com>
2020-09-20 17:32:13 -0300

    cambio estilos y modifico fun draw()

--- App.js
+++ App.js
@@ -15,10 +15,11 @@ var x = canvas.width / 2;
 var y = canvas.height - 30;
 var dx = 2;
 var dy = -2;
+var radioCirculo = 10;
 
 function drawBola() {
     ctx.beginPath();
-    ctx.arc(x, y, 10, 0, Math.PI*2);
+    ctx.arc(x, y, radioCirculo, 0, Math.PI*2);
     ctx.fillStyle = "#0095DD";
     ctx.fill();
     ctx.closePath();
@@ -28,6 +29,11 @@ function draw() {
     // borro contenido de lienzo antes de dijubar denuevo:
     ctx.clearRect(0, 0, canvas.width, canvas.height);   
     drawBola();
+    if (x + dx > (canvas.width - radioCirculo) || x + dx < radioCirculo) 
+        dx = -dx;
+
+    if (y + dy > (canvas.height - radioCirculo) || y + dy < radioCirculo) 
+        dy = -dy;
     // invremento posiciones x,y en 2, -2 :
     x += dx;
     y += dy;
